3c07f8ea80d52954600cc3f70d37906b7b5f0650
Andrei <andrei2.barlea@live.uwe.ac.uk>
2020-10-11 11:55:05 +0100

    media queries & transitions

--- script.js
+++ script.js
@@ -2,12 +2,18 @@ $(document).ready(function()
 {
     $(window).scroll(function()
     {
-        if(this.scrollY >20)
+        if(this.scrollY > 20)
         {
             $('.navbar').addClass("sticky");
         } else
         {
-
+            $('.navbar').removeClass("sticky");
         }
+    })
+    //menu toggle / navbar
+    $('.menu-btn').click(function()
+    {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
     })
 });
