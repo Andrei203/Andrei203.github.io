4f7ecf6305076c3c9144adc3e0949cbc2c8fdcf5
Andrei <andrei2.barlea@live.uwe.ac.uk>
2020-10-20 15:24:36 +0100

    finito

--- blog.js
+++ blog.js
@@ -5,22 +5,42 @@ const BLOGS = [
     body: "hello im andrei",
     date: "20/10/2020",
   },
+  {
+    title: "Coursework",
+    image: "/images/1200x500.png",
+    body: "hello im andrei",
+    date: "20/10/2020",
+  },
   
 ];
 $(function () {
   var blogcontainer = document.getElementById("blogs");
     BLOGS.forEach((blog) => {
      var b = `
-         <div class = "blog">
-            <div class = "title">${blog.title} </div>
-            <div class = "date"> ${blog.date} </div>
-            <div class = "image-container">
-                <img src = "${blog.image}"/>
+          <div class = "blog">
+            <div class="row">
+              <div class="col-md-6">
+                <div class = "title">${blog.title}</div>
+              </div>
+              <div class="col-md-6">
+                <div class = "date"> ${blog.date}</div>
+              </div>
             </div>
-            <div class = "body">
-              ${blog.body}
+            <div class="row">
+              <div class="col-md-12">
+                <div class = "image-container">
+                  <img src = "${blog.image}"/>
+                </div>
+              </div>
             </div>
-        </div>
+            <div class="row">
+              <div class="col-md-10 offset-md-1">
+                <div class = "body">
+                  ${blog.body}
+                </div>
+              </div>
+            </div>
+          </div>
     `;
      blogcontainer.innerHTML += b;
   });
